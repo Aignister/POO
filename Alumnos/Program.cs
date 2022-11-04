46f187a3372ffddaa9b1627cfe6bc36cc25a70f3
Enrique Torres <enriquetorresv652@gmail.com>
2022-11-03 17:50:35 -0700

    Update Program.cs

--- Alumnos/Program.cs
+++ Alumnos/Program.cs
@@ -1,2 +1,80 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Alumno
+{
+    public string Nombre;
+    public string Apellido;
+    public string NumControl;
+
+    public Alumno(string nombre, string apellido, string numControl)
+    {
+        Nombre = nombre;
+        Apellido = apellido;
+        NumControl = numControl;
+    }
+
+    public override string ToString()
+    {
+        return String.Format($"{NumControl} {Nombre} {Apellido}");
+    }
+
+    public void Imprime()
+    {
+        Console.WriteLine($"{NumControl} {Nombre} {Apellido}");
+    }
+}
+
+class Licenciatura : Alumno
+{
+    protected string Servicio;
+    protected string Recidencia;
+
+    public Licenciatura(string nombre, string apellido, string numControl, string servicio, string recidencia):base(nombre, apellido, numControl)
+    {
+        Servicio = servicio;
+        Recidencia = recidencia;
+    }
+    
+    public override string ToString()
+    {
+        return String.Format($"{base.ToString()} {Servicio} {Recidencia}");
+    }
+
+    public new void Imprime()
+    {
+        Console.WriteLine($"{NumControl} {Nombre} {Apellido} {Servicio} {Recidencia}");
+    }
+}
+
+class Posgrado : Alumno
+{
+    protected string Investigacion;
+
+    public Posgrado(string nombre, string apellido, string numControl, string investigacion):base(nombre, apellido, numControl)
+    {
+        Investigacion = investigacion;
+    }
+    
+    public override string ToString()
+    {
+        return String.Format($"{base.ToString()} {Investigacion}");
+    }
+    public new void Imprime()
+    {
+        Console.WriteLine($"{NumControl} {Nombre} {Apellido} {Investigacion}");
+    }
+}
+
+namespace Herencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Alumno a = new Alumno("Azir", "Shurit", "652989A");
+            Licenciatura lic = new Licenciatura("Zoe", "Star", "2325742B", "Cyber", "Google");
+            Posgrado pos = new Posgrado("Dante", "Mojica", "242120022", "Alacranes");
+            a.Imprime();
+            lic.Imprime();
+            pos.Imprime();
+        }
+    }
+}

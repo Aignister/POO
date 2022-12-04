2c89b34e804be1031a750bc4a10fc56d34bf8f97
Enrique Torres <enriquetorresv652@gmail.com>
2022-12-03 18:54:19 -0800

    Update Program.cs

--- Archivos/Program.cs
+++ Archivos/Program.cs
@@ -3,113 +3,92 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
-class Producto
+class Product
 {
-    public string codigo {get; set;}
-    public string descripcion {get; set;}
-    public int departamento {get; set;}
-    public int likes {get; set;}
-    public decimal precio {get; set;}
+  public string code {get; set;}
+  public string description {get; set;}
+  public int departament {get; set;}
+  public int likes {get; set;}
+  public decimal price {get; set;}
 
-    public Producto(string codigo, string descripcion, int departamento, int likes, decimal precio)
-    {
-      this.codigo = codigo;
-      this.descripcion = descripcion;
-      this.departamento = departamento;
-      this.likes = likes;
-      this.precio = precio;
-    }
+  public Product(string code, string description, int departament, int likes, decimal price)
+  {
+    this.code = code;
+    this.description = description;
+    this.departament = departament;
+    this.likes = likes;
+    this.price = price;
+  }
 
     public override string ToString()
     {
-        return String.Format($"Codigo: {codigo} Descripcion: {descripcion} Departamento: {departamento} Likes: {likes} Precio: {precio}");
+        return String.Format($"{code} {description} {departament} {likes} {price}");
     }
 }
 
-class ProductoDB
+class ProductDB
 {
-    public static void SaveText(List<Producto> productos, String path)
-    {
-      StreamWriter sw = new StreamWriter(new FileStream(path, FileMode.OpenOrCreate, FileAccess.Write));
+  public static void SaveText(List<Product> products, String path)
+  {
+    StreamWriter sw = new StreamWriter(new FileStream(path, FileMode.OpenOrCreate, FileAccess.ReadWrite));
 
-      foreach (var producto in productos )
-      {
-        sw.WriteLine($"{producto.codigo}|{producto.descripcion}|{producto.departamento}|{producto.likes}|{producto.precio}");
-      }
-      sw.Close();
-    }
-   
-    public static void SaveBin(List<Producto> products, String path)
+    foreach (var product in products)
     {
-      BinaryWriter bw = new BinaryWriter(new FileStream(path, FileMode.OpenOrCreate, FileAccess.Write));
-      foreach(var product in products)
-      {
-        bw.Write(product.codigo);
-        bw.Write(product.descripcion);
-        bw.Write(product.departamento);
-        bw.Write(product.likes);
-        bw.Write(product.precio);
-      }
-      bw.Close();
+      sw.WriteLine($"{product.code}|{product.description}|{product.departament}|{product.likes}|{product.price}");
     }
+    sw.Close();
+  }
 
-    public static List<Product> GetText(String path)
+  public static void SaveBin(List<Product> productos, String path)
+  {
+    BinaryWriter bw = new BinaryWriter(new FileStream(path, FileMode.OpenOrCreate, FileAccess.ReadWrite));
+
+    foreach (var producto in productos)
     {
-       List<Product> products = new();
-       StreamReader sr = new StreamReader(
-          new FileStream(path, FileMode.Open, FileAccess.Read)
-          );
-       
-       while ( sr.Peek() != -1)
-       {
-          String? row = sr.ReadLine();
-          String[] columns = row.Split("|");
-          products.Add(new Product(columns[0], columns[1], Decimal.Parse(columns[2])));
-       }
-        sr.Close();
-
-        return products;
+      bw.Write(producto.code);
+      bw.Write(producto.description);
+      bw.Write(producto.departament);
+      bw.Write(producto.likes);
+      bw.Write(producto.price);
     }
-   
-    public static List<Product> GetBin(String path)
+    bw.Close();
+  }
+
+  public static List<Product> GetText(String path)
+  {
+    List<Product> products = new();
+    StreamReader sr = new StreamReader(new FileStream(path, FileMode.Open, FileAccess.Read));
+
+    while(sr.Peek() != -1)
     {
-       List<Product> products = new();
-       BinaryReader br = new BinaryReader(
-          new FileStream(path, FileMode.Open, FileAccess.Read)
-          );
-       
-       while ( br.PeekChar() != -1)
-       {
-         products.Add( new Product(
-               br.ReadString(), br.ReadString(), br.ReadDecimal()
-               ));
-       }
-        br.Close();
-
-        return products;
+      String? row = sr.ReadLine();
+      String[] columns = row.Split("|");
+      products.Add(new Product(columns[0], columns[1], int.Parse(columns[2]), int.Parse(columns[3]), Decimal.Parse(columns[4])));
     }
+    sr.Close();
+    
+    return products;
+  }
+
+  public static List<Product> GetBin(String path)
+  {
+    List<Product> products = new();
+    BinaryReader br = new BinaryReader(new FileStream(path, FileMode.Open, FileAccess.Read));
+
+    while(br.PeekChar() != -1)
+    {
+      products.Add(new Product(br.ReadString(), br.ReadString(), br.ReadInt32(), br.ReadInt32(), br.ReadDecimal()));
+    }
+    br.Close();
+
+    return products;
+  }
 }
-
 
 class Program
 {
-    static void Main()
-    {
-        Console.WriteLine("Lista de Productos");
-        List<Product> products = new();
-        products.Add( new Product("AAA", "FIFA 2023 XBOX", 65.23m));
-        products.Add( new Product("AAB", "FIFA 2023 Play", 65.23m));
-        products.Add( new Product("AAC", "FIFA 2022", 55.23m));
-   
-        foreach(var p in products)
-          Console.WriteLine(p);
-        Console.WriteLine();
-
-       //ProductDB.SaveBin(products, @"productos.bin");
-       /* List<Product> productsArchivo =  ProductDB.GetText(@"productos.txt");
-*/      
-        List<Product> productsArchivo =  ProductDB.GetBin(@"productos.bin");
-        foreach(var p in productsArchivo)
-          Console.WriteLine(p);
-    }
+  static void Main()
+  {
+    Console.WriteLine("Lista de productos");
+  }
 }

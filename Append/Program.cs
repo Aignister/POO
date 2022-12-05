6fcd213c3c769f56554597ae02ea3b9eaa82479b
Enrique Torres <enriquetorresv652@gmail.com>
2022-12-04 22:10:26 -0800

    append

---
+++ Append/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading;
+using System.IO;
+using System.Linq;
+using System.Text;
+class Program
+{
+    static void Main()
+    {
+        using (StreamWriter w = File.AppendText(@"productapp.txt"));
+        {
+            w.write("Lista de productos:");
+            Console.WriteLine("Prueba de append");
+        }
+        Console.Read();
+    }
+}
